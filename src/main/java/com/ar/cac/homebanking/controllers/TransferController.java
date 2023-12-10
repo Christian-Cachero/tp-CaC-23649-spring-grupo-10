7502b547a1bb220f1576191c2a6d83c646cdf71f
Christian Cachero <Ch_canchero@live.com.ar>
2023-12-10 16:20:06 -0300

    Refactorizacion del código para dividir la capa de servicios en interfaces y sus implementaciones, además de acomodar un poco el código para poder trabajar con los Optionals.

--- src/main/java/com/ar/cac/homebanking/controllers/TransferController.java
+++ src/main/java/com/ar/cac/homebanking/controllers/TransferController.java
@@ -1,48 +1,57 @@
 package com.ar.cac.homebanking.controllers;
 
 import com.ar.cac.homebanking.models.dtos.TransferDTO;
-import com.ar.cac.homebanking.services.TransferService;
+import com.ar.cac.homebanking.models.dtos.TransferDTO;
+import com.ar.cac.homebanking.services.implementation.TransferServiceImp;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/transfers")
 public class TransferController {
 
-    private final TransferService service;
+    private final TransferServiceImp service;
 
-    public TransferController(TransferService service){
+    public TransferController(TransferServiceImp service){
         this.service = service;
     }
 
     @GetMapping
     public ResponseEntity<List<TransferDTO>> getTransfers(){
-        List<TransferDTO> transfers = service.getTransfers();
-        return ResponseEntity.status(HttpStatus.OK).body(transfers);
+        Optional<List<TransferDTO>> TransferListOptional = service.getTransfers();
+
+        if(TransferListOptional.isPresent()){
+            List<TransferDTO> TransferList = TransferListOptional.get();
+            return new ResponseEntity<>(TransferList, HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<TransferDTO> getTransferById(@PathVariable Long id){
-        TransferDTO transfer = service.getTransferById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(transfer);
+        return service.getTransferById(id).map(t -> new ResponseEntity<>(t, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 
     @PostMapping
     public ResponseEntity<TransferDTO> performTransfer(@RequestBody TransferDTO dto){
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.performTransfer(dto));
+       return service.performTransfer(dto).map(t -> new ResponseEntity<>(t, HttpStatus.CREATED))
+                .orElse(new ResponseEntity<>(HttpStatus.CONFLICT));
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<TransferDTO> updateTransfer(@PathVariable Long id, @RequestBody TransferDTO transfer){
-        return ResponseEntity.status(HttpStatus.OK).body(service.updateTransfer(id, transfer));
+    public ResponseEntity<TransferDTO> updateTransfer(@PathVariable Long id, @RequestBody TransferDTO dto){
+        return service.updateTransfer(id, dto).map(t -> new ResponseEntity<>(t, HttpStatus.ACCEPTED))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<String> deleteTransfer(@PathVariable Long id){
-        return ResponseEntity.status(HttpStatus.OK).body(service.deleteTransfer(id));
+        return new ResponseEntity<>(service.deleteTransfer(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/ar/cac/homebanking/services/implementation/LoanServiceImp.java
+++ src/main/java/com/ar/cac/homebanking/services/implementation/LoanServiceImp.java
@@ -1,8 +1,8 @@
-package com.ar.cac.homebanking.services;
+package com.ar.cac.homebanking.services.implementation;
 
 import com.ar.cac.homebanking.exceptions.AccountNotFoundException;
-import com.ar.cac.homebanking.exceptions.InsufficientFoundsException;
 import com.ar.cac.homebanking.exceptions.LoanNotFoundException;
+import com.ar.cac.homebanking.mappers.AccountMapper;
 import com.ar.cac.homebanking.mappers.LoanMapper;
 import com.ar.cac.homebanking.models.Account;
 import com.ar.cac.homebanking.models.Loan;
@@ -10,43 +10,48 @@ import com.ar.cac.homebanking.models.dtos.LoanDTO;
 import com.ar.cac.homebanking.models.enums.LoanStatus;
 import com.ar.cac.homebanking.repositories.AccountRepository;
 import com.ar.cac.homebanking.repositories.LoanRepository;
+import com.ar.cac.homebanking.services.abstraction.LoanService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.math.BigDecimal;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-public class LoanService {
+public class LoanServiceImp implements LoanService {
 
     private final LoanRepository repository;
     private final AccountRepository accountRepository;
 
-    public LoanService(LoanRepository repository, AccountRepository accountRepository) {
+    public LoanServiceImp(LoanRepository repository, AccountRepository accountRepository) {
         this.repository = repository;
         this.accountRepository = accountRepository;
     }
 
-    public List<LoanDTO> getLoans() {
+    public Optional<List<LoanDTO>> getLoans() {
         List<Loan> loans = repository.findAll();
-        return loans.stream()
+        return Optional.of( loans.stream()
                 .map(LoanMapper::loanToDto)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 
-    public LoanDTO getLoanById(Long id) {
-        Loan loan = repository.findById(id).orElseThrow(() ->
+    public Optional<LoanDTO> getLoanById(Long id) {
+        return Optional.of(repository.findById(id).map(LoanMapper::loanToDto)
+                .orElseThrow(() ->
+                new LoanNotFoundException("Cuenta no encontrada con id : " + id)));
+
+        /*Loan loan = repository.findById(id).orElseThrow(() ->
                 new LoanNotFoundException("Cuenta no encontrada con id : " + id));
-        return LoanMapper.loanToDto(loan);
+        return LoanMapper.loanToDto(loan);*/
     }
 
-    public LoanDTO updateLoan(Long id, LoanDTO loanDto) {
+    public Optional<LoanDTO> updateLoan(Long id, LoanDTO loanDto) {
         Loan loan = repository.findById(id).orElseThrow(() ->
                 new LoanNotFoundException("Cuenta no encontrada con id : " + id));
         Loan updatedLoan = LoanMapper.dtoToLoan(loanDto);
         updatedLoan.setId(loan.getId());
-        return LoanMapper.loanToDto(repository.save(updatedLoan));
+        return Optional.of(LoanMapper.loanToDto(repository.save(updatedLoan)));
     }
 
     public String deleteLoan(Long id) {
@@ -59,10 +64,11 @@ public class LoanService {
     }
 
     @Transactional
-    public LoanDTO createLoan(LoanDTO loanDto) {
+    public Optional<LoanDTO> createLoan(LoanDTO loanDto) {
         // Comprobar si la cuenta existe
         Account account = accountRepository.findById(loanDto.getUserId())
-                .orElseThrow(() -> new AccountNotFoundException("Cuenta no encontrada con id : " + loanDto.getUserId()));
+                .orElseThrow(() -> new AccountNotFoundException("Cuenta no encontrada con id : "
+                        + loanDto.getUserId()));
 
         // Crear el préstamo y asignarlo a la cuenta
         Loan loan = LoanMapper.dtoToLoan(loanDto);
@@ -77,6 +83,6 @@ public class LoanService {
         accountRepository.save(account);
 
         // Devolver el DTO del préstamo con información detallada
-        return LoanMapper.loanToDto(loan);
+        return Optional.of(LoanMapper.loanToDto(loan));
     }
 }
